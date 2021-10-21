19051b36ae483ff77553475c940052d4686c52c6
Casey Muratori <casey@mollyrocket.com>
2021-10-21 01:00:15 -0700

    Fixed a bug where the old (non-Unicode) version of PostMessage was being called, which causes problems for IMEs

--- refterm.c
+++ refterm.c
@@ -63,7 +63,7 @@ static LRESULT CALLBACK WindowProc(HWND Window, UINT Message, WPARAM WParam, LPA
         case WM_CHAR:
         case WM_SIZE:
         {
-            PostThreadMessage(RenderThreadID, Message, WParam, LParam);
+            PostThreadMessageW(RenderThreadID, Message, WParam, LParam);
         } break;
 
         default:
@@ -144,7 +144,7 @@ void WinMainCRTStartup()
            (Message.message == WM_QUIT) ||
            (Message.message == WM_SIZE))
         {
-            PostThreadMessage(RenderThreadID, Message.message, Message.wParam, Message.lParam);
+            PostThreadMessageW(RenderThreadID, Message.message, Message.wParam, Message.lParam);
         }
         else
         {
